765787c0e61ac6fe5fe84982c5442f40520a096b
goldmonkey777 <contact@goldmonkey.studio>
2026-02-26 15:18:30 +0100

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,9 @@
-function Navbar() {}
+function Navbar() {
+  return (
+    <nav className="navbar navbar-dark bg-primary mb-3 px-3">
+      <span className="navbar-brand">WikiCountries</span>
+    </nav>
+  );
+}
 
 export default Navbar;
